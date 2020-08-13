cba5f95bfd02c306980aa631ba939e0936a23d5f
Timothy Sharkey <Techne3@gmail.com>
2020-08-13 16:29:12 -0600

    created redux store to maintain state set up actions/reducers

--- client/src/Screens/HomeScreen.js
+++ client/src/Screens/HomeScreen.js
@@ -1,47 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import axios from "axios";
-// import AxiosApi from "../api/AxiosApi";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen(props) {
-  const [products, setProducts] = useState([]);
-
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
   useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchData();
+    dispatch(listProducts());
   }, []);
 
-  return (
-    <div>
-      <ul className="products">
-        {products.map((product) => (
-          <li key={product.id}>
-            <div className="product">
-              <Link to={`/product/${product.id}`}>
-                <img
-                  className="product-image"
-                  src={product.image}
-                  alt="product"
-                />
-              </Link>
-              <div className="product-name">
-                <Link to={`/product/${product.id}`}> {product.name} </Link>
-              </div>
-              <div className="product-brand"> {product.brand} </div>
-              <div className="product-price"> {product.price} </div>
-              <div className="product-rating">
-                {" "}
-                {product.rating} Stars {product.numReviews}{" "}
-              </div>
+  return loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <ul className="products">
+      {products.map((product) => (
+        <li key={product._id}>
+          <div className="product">
+            <Link to={"/product/" + product._id}>
+              <img
+                className="product-image"
+                src={product.image}
+                alt="product"
+              />
+            </Link>
+            <div className="product-name">
+              <Link to={"/product/" + product._id}>{product.name}</Link>
             </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+            <div className="product-brand">{product.brand}</div>
+            <div className="product-price">${product.price}</div>
+            <div className="product-rating">
+              {product.rating} Stars ({product.numReviews} Reviews)
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
-
 export default HomeScreen;
