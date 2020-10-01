78267f69515f0bc76c22fa40b69e58426b4acabf
Timothy Sharkey <Techne3@gmail.com>
2020-10-01 11:56:02 -0600

    created  register route for newUsers -fixed getToken function in utils to return user data -added bodyParser depen.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,27 +1,44 @@
 import express from "express";
 import User from "../models/userModel";
+import { getToken } from "../util";
 
 const router = express.Router();
 
 router.post("/signin", async (req, res) => {
-  try {
-    const signinUser = await User.findOne({
-      email: req.body.email,
-      password: req.body.password,
+  const signinUser = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (signinUser) {
+    res.send({
+      _id: signinUser.id,
+      name: signinUser.name,
+      email: signinUser.email,
+      isAdmin: signinUser.isAdmin,
+      token: getToken(signinUser),
     });
-    if (signinUser) {
-      res.send({
-        _id: signinUser.id,
-        name: signinUser.name,
-        email: signinUser.email,
-        isAdmin: signinUser.isAdmin,
-        token: getToken(user),
-      });
-    } else {
-      res.status(401).send({ msg: "Invalid Email or password" });
-    }
-  } catch (error) {
-    res.send({ msg: error.message });
+  } else {
+    res.status(401).send({ msg: "Invalid Email or Password." });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  const newUser = await user.save();
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser),
+    });
+  } else {
+    res.status(401).send({ msg: "Invalid User Data." });
   }
 });
 
@@ -29,7 +46,7 @@ router.get("/createadmin", async (req, res) => {
   try {
     const user = new User({
       name: "Tim",
-      email: "a.com",
+      email: "a@gmail.com",
       password: "123",
       isAdmin: true,
     });
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import data from "./data";
-import config from "./config";
 import dotenv from "dotenv";
+import config from "./config";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import userRoute from "./routes/userRoutes";
 
-// mongo db connection
 dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
@@ -18,12 +18,11 @@ mongoose
   .catch((error) => console.log(error.reason));
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use("/api/users", userRoute);
-
 app.get("/api/products/:id", (req, res) => {
   const productId = req.params.id;
-  const product = data.products.find((x) => x.id === productId);
+  const product = data.products.find((x) => x._id === productId);
   if (product) res.send(product);
   else res.status(404).send({ msg: "Product Not Found." });
 });
