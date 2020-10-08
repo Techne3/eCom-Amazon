494702b1c455c48a1590326cbb19b53222bc1749
Timothy Sharkey <Techne3@gmail.com>
2020-10-08 13:53:56 -0600

    new action and reducer to save the products - if the Authorization token is in header

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -5,6 +5,9 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  PRODUCT_SAVE_REQUEST,
+  PRODUCT_SAVE_SUCCESS,
+  PRODUCT_SAVE_FAIL,
 } from "../constants/productConstants";
 import axios from "axios";
 
@@ -17,7 +20,24 @@ const listProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
   }
 };
-
+//////////
+const saveProduct = (product) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
+    const {
+      userSignin: { userInfo },
+    } = getState();
+    const { data } = await axios.post("/api/products", product, {
+      headers: {
+        Authorization: "Bearer" + userInfo.token,
+      },
+    });
+    dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: PRODUCT_SAVE_FAIL, payload: error.message });
+  }
+};
+////////////
 const detailsProduct = (productId) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
@@ -28,4 +48,4 @@ const detailsProduct = (productId) => async (dispatch) => {
   }
 };
 
-export { listProducts, detailsProduct };
+export { listProducts, detailsProduct, saveProduct };
--- client/src/reducer/productReducers.js
+++ client/src/reducer/productReducers.js
@@ -5,6 +5,9 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  PRODUCT_SAVE_REQUEST,
+  PRODUCT_SAVE_SUCCESS,
+  PRODUCT_SAVE_FAIL,
 } from "../constants/productConstants";
 
 function productListReducer(state = { products: [] }, action) {
@@ -32,5 +35,17 @@ function productDetailsReducer(state = { product: {} }, action) {
       return state;
   }
 }
+function productSaveReducer(state = { product: {} }, action) {
+  switch (action.type) {
+    case PRODUCT_SAVE_REQUEST:
+      return { loading: true };
+    case PRODUCT_SAVE_SUCCESS:
+      return { loading: false, success: true, product: action.payload };
+    case PRODUCT_SAVE_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+}
 
-export { productListReducer, productDetailsReducer };
+export { productListReducer, productDetailsReducer, productSaveReducer };
